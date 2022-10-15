ba7de68be65948e73465d5ff54ac95b5d2e17980
paul-ooi <info@paulooidesign.com>
2022-10-15 00:42:03 -0400

    add menu transition

--- src/js/_menu.js
+++ src/js/_menu.js
@@ -1,12 +1,14 @@
 const navToggle = () => {
   console.log('menu init');
   const btn = document.querySelector('.header__menu-button');
+  const menu = document.querySelector('nav.header__menu');
   console.log(btn); 
-  if (btn) {
+  if (btn && menu) {
     btn.addEventListener('click', () => {
       let state = btn.getAttribute('aria-expanded');
       let newState = (state === 'true') ? 'false' : 'true';
       btn.setAttribute('aria-expanded', newState);
+      menu.classList.toggle('header__menu--hidden')
     })
   }
 }
