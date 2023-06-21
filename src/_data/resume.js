82da9e337fd8ea2e8e29972f5d10086c0cb1d827
paul-ooi <info@paulooidesign.com>
2023-06-21 01:06:38 -0400

    add resume partials for initial data

---
+++ src/_data/resume.js
@@ -0,0 +1,55 @@
+const skills = {
+  software : [
+    "Visual Studio Code IDE",
+    "Git & Github",
+    "SQL Developer",
+    "Visual Studio",
+    "Adobe Photoshop",
+    "Adobe Illustrator",
+    "Adobe InDesign",
+    "Google Sketchup",
+  ],
+  "skills": [
+    "HTML5",
+    "CSS/LESS",
+    "Bootstrap 4",
+    "JavaScript/jQuery",
+    "PHP",
+    "XML",
+    "MySQL/Oracle SQL",
+    "C#",
+    "3D Modeling",
+    "Web Development",
+    "Graphic Design",
+    "Interior Design",
+    "Industrial Design",
+  ],
+  "soft skills": [
+    "Responsible",
+    "Adaptable",
+    "Self-Motivated",
+    "Professional",
+    "Team Player",
+    "Patient Communicator",
+  ]
+}
+
+const platform = [
+  {
+    text: "Fork me on Github",
+    url: "https://github.com/paul-ooi"
+  },
+  {
+    text: "Follow me on Behance",
+    url: "https://www.behance.net/paulooidesign"
+  },
+  {
+    text: "Connect with me on LinkedIn",
+    url: "https://linkedin.com/in/paulooi"
+  },
+]
+
+module.exports = {
+  skills,
+  platform
+}
