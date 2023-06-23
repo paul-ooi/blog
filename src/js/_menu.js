64bf4a6939c788f4b847f29a9886b04276b10e49
paul-ooi <info@paulooidesign.com>
2023-06-23 04:40:42 -0400

    simplify menu ternary

--- src/js/_menu.js
+++ src/js/_menu.js
@@ -1,12 +1,10 @@
 const navToggle = () => {
-  console.log('menu init');
   const btn = document.querySelector('.header__menu-button');
   const menu = document.querySelector('nav.header__menu');
-  console.log(btn); 
   if (btn && menu) {
     btn.addEventListener('click', () => {
       let state = btn.getAttribute('aria-expanded');
-      let newState = (state === 'true') ? 'false' : 'true';
+      let newState = !(state === 'true');
       btn.setAttribute('aria-expanded', newState);
       menu.classList.toggle('header__menu--hidden');
       document.body.classList.toggle('show-overlay')
