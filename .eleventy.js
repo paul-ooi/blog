9c013fdb91d36ed70863f2e0ea5cb831e73c93b0
paul-ooi <info@paulooidesign.com>
2023-10-27 23:11:09 -0400

    try different base url

--- .eleventy.js
+++ .eleventy.js
@@ -51,7 +51,7 @@ module.exports = function(eleventyConfig) {
             layouts: '_includes/layouts',
             output: 'public'
         },
-        pathPrefix: process.env.NODE_ENV !== 'production' ? '/' : '/blog/',
+        pathPrefix: process.env.NODE_ENV !== 'production' ? '/' : '/',
         markdownTemplateEngine: "njk",
         htmlTemplateEngine: "njk",
         dataTemplateEngine: "njk",
