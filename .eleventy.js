aaeb707e20f883add52f1e281564e9f82f4c9a7c
paul-ooi <info@paulooidesign.com>
2023-10-27 21:26:53 -0400

    use pathPrefix

--- .eleventy.js
+++ .eleventy.js
@@ -39,10 +39,10 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets/images");
     eleventyConfig.addPassthroughCopy("src/dist");
     eleventyConfig.addPlugin(syntaxHighlight);
-    eleventyConfig.addPlugin(EleventyHtmlBasePlugin, {
-        baseHref: process.env.NODE_ENV !== 'production' ? '/' : '/blog/',
-        extensions: "njk,html,css,md",
-    });
+    // eleventyConfig.addPlugin(EleventyHtmlBasePlugin, {
+    //     baseHref: process.env.NODE_ENV !== 'production' ? '/' : '/blog/',
+    //     extensions: "njk,html,css,md",
+    // });
 
     octiconMenu(eleventyConfig)
     octiconClose(eleventyConfig)
@@ -54,6 +54,7 @@ module.exports = function(eleventyConfig) {
             layouts: '_includes/layouts',
             output: 'public'
         },
+        pathPrefix: process.env.NODE_ENV !== 'production' ? '/' : '/blog/',
         markdownTemplateEngine: "njk",
         htmlTemplateEngine: "njk",
         dataTemplateEngine: "njk",
